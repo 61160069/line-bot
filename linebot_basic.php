19acf999537ff10bdee09535764f0f260d49d07b
61160069 <66722210+61160069@users.noreply.github.com>
2020-10-29 16:05:58 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'ZLuBBM5rRSE3lC/vDBCFhFOH6WLlOACI/VWOfuUJcFFAeOSaffZHWTprZ97SMSrLZmDppZ5Av307PL8H+jTmq5NCACSqYO1SwmK5Nziax4OcRlzP4jHPOQXo4RQ8wY+M1+uQsNvzJkIdsT/0XdtOZgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
